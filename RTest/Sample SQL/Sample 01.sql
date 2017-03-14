ec4ac355dbfccf2b0413b4d5a981fb60ee0ebac1
Babu Reddy <babureddyh@hotmail.com>
2017-03-14 21:40:18 +0530

    Initial Checkin

---
+++ RTest/Sample SQL/Sample 01.sql
@@ -0,0 +1,15 @@
+﻿-- Using R Code in Transact-SQL (SQL Server R Services)
+-- Am using Visual Studio with R Tools plugin
+-- You must have access to an instance of SQL Server where R Services is already installed, for all the samples in this project.
+
+-- Create a sqlquery file. Connect to the database server and setup the database as you would do in SSMS.
+-- Execute the below query. 
+-- If all went through fine, then you have executed your first R code by calling it using sp_execute_external_script and wrapping
+-- your R code within it.
+
+EXEC sp_execute_external_script  
+  @language =N'R',    
+  @script=N'OutputDataSet<-InputDataSet',      
+  @input_data_1 =N'SELECT 1 AS hello'    
+  WITH RESULT SETS (([hello] int not null));    
+GO    
---
+++ RTest/Sample SQL/Sample 02.sql
@@ -0,0 +1,19 @@
+﻿/* --------- Working with Inputs and Outputs -------------
+
+When you want to run R code in SQL Server, you must wrap the R script in a system stored procedure, sp_execute_external_script. 
+This stored procedure is used to start the R runtime in the context of SQL Server, which passes data to R, 
+manages R user sessions securely, and returns any results to the client.
+
+*/
+
+-- Create a test database
+-- Create a small table of test data by running the following T-SQL statement
+
+CREATE TABLE RTestData ([col1] int not null) ON [PRIMARY]    
+INSERT INTO RTestData   VALUES (1);    
+INSERT INTO RTestData   VALUES (10);    
+INSERT INTO RTestData   VALUES (100) ;    
+GO
+
+-- Now that the table has been created and data inserted, let us access the data using R script
+-- run the following statement. It gets the data from the table, makes a round-trip through the R runtime, and returns the values with the column name, NewColName.
